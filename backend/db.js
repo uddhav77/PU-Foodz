d6d3fe807848679eed31c73a5245c1d04ca6ace1
Uddhav <uddhavlk@gmail.com>
2023-07-27 15:24:19 +0545

    Project initialization

---
+++ backend/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const mongoURI =
+  "mongodb+srv://uddhav7:Meronaldo7@cluster0.ppe8whi.mongodb.net/goFood?retryWrites=true&w=majority";
+
+const mongoDB = async () => {
+  try {
+    await mongoose.connect(mongoURI, { useNewUrlParser: true });
+    console.log("Connected successfully");
+
+    // Fetch data from the "food_items" collection
+    const collection = mongoose.connection.db.collection("food_items");
+    const data = await collection.find({}).toArray();
+    console.log();
+  } catch (error) {
+    console.error("Connection failed:", error);
+  }
+};
+
+module.exports = mongoDB;
