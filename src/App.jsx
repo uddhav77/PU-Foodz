dbb1139252bc61cd2022c0d39827375661092aeb
Uddhav <uddhavlk@gmail.com>
2023-07-28 09:22:31 +0545

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Order from "./pages/Order";
-import NavBar from "./components/NavBar";
-// import Post from "./pages/Post";
-// import Todo from "./pages/Todo";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 
@@ -13,8 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/order" element={<Order />} />
-        {/* <Route path="/post/:number" element={<Post />} />
-        <Route path="/todo" element={<Todo />} /> */}
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
