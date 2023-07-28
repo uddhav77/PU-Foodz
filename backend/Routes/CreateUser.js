0e145abfc2c2d4275e5b258dea57b5a6d1bb7b35
Uddhav <uddhavlk@gmail.com>
2023-07-28 09:17:56 +0545

    SignUp validation-Backend

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -1,20 +1,34 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
-router.post("/createuser", async (req, res) => {
-  try {
-    await User.create({
-      name: req.body.name,
-      password: req.body.password,
-      email: req.body.email,
-      location: req.body.location,
-    });
-    res.json({ sucess: true });
-  } catch (error) {
-    console.log(error);
-    res.json({ sucess: false });
+router.post(
+  "/createuser",
+  [
+    body("email").isEmail(),
+    body("password", "Password must be alteast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      await User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+        location: req.body.location,
+      });
+      res.json({ sucess: true });
+    } catch (error) {
+      console.log(error);
+      res.json({ sucess: false });
+    }
   }
-});
+);
 
 module.exports = router;
