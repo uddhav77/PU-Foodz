af6288ed394a905f582bf5516cd82212bc1750ce
myselfregmi <pushparegmi0@gmail.com>
2023-07-29 11:21:00 +0545

    Categories and About us

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
-const Services = ({ image, titleName }) => {
+const Services = ({ image, titleName, content }) => {
   return (
     <div>
-      <img src={image} className="w-[200px] h-[200px]" />
-      <h1 className="text-4xl text-red-600 font-bold text-center mt-8">
-        {titleName}
-      </h1>
+      <div className="bg-white flex flex-col cursor-pointer  transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-cyan-500 duration-300 items-center flex-1 p-14 shadow-2xl rounded-3xl">
+        <img src={image} className="w-[200px] h-[200px]" />
+        <h1 className="text-4xl text-red-500 font-bold text-center mt-8">
+          {titleName}
+        </h1>
+        <h1 className="text-2xl text-center mt-6">{content} </h1>
+      </div>
     </div>
   );
 };
