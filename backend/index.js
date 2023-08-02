ecdb62389c34cb599a2150d2a5207b6077670856
Uddhav <uddhavlk@gmail.com>
2023-08-02 14:00:02 +0545

    OrderSchema

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,7 @@ app.get("/", (req, res) => {
 
 app.use("/api", require("./Routes/CreateUser"));
 app.use("/api", require("./Routes/DisplayData"));
+app.use("/api", require("./Routes/OrderData"));
 app.use("/api", require("./Routes/FoodData"));
 
 app.listen(port, () => {
