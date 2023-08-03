845805492d749ac97b01a90c14fe1a3432899d26
myselfregmi <pushparegmi0@gmail.com>
2023-08-03 12:44:42 +0545

    MyCart CSS

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { AiFillDelete } from "react-icons/ai";
 import { useCart, useDispatchCart } from "../components/ContextReducer";
+
 export default function Cart() {
   let data = useCart();
   let dispatch = useDispatchCart();
+
   if (data.length === 0) {
     return (
-      <div>
-        <div className="text-6xl text-center mt-96 text-white font-bold  ">
-          The Cart is Empty!
-        </div>
+      <div className="text-6xl text-center mt-96 text-white font-bold">
+        The Cart is Empty!
       </div>
     );
   }
@@ -43,76 +43,52 @@ export default function Cart() {
   };
 
   let totalPrice = data.reduce((total, food) => total + food.price, 0);
-  return (
-    <div>
-      {console.log(data)}
-      <div className="p-20 flex flex-col items-center justify-center">
-        <div className=" flex flex-col justify-center">
-          <div className="text-white text-3xl flex items-center p-8 ">
-            {data.map((food, index) => (
-              <div className="flex gap-44 text-4xl text-black ">
-                <div className="flex flex-col items-center ">
-                  <p className="font-bold font-cursive text-red-700 text-5xl">
-                    #
-                  </p>
-                  <div className="mt-12">{index + 1}</div>
-                </div>
-                <div className="flex flex-col items-center ">
-                  <p className="font-bold font-cursive  text-red-700 text-5xl">
-                    Name
-                  </p>
-                  <div className="mt-12 ">{food.name}</div>
-                </div>
-                <div className="flex flex-col items-center">
-                  <p className="font-bold font-cursive text-red-700 text-5xl">
-                    {" "}
-                    Quantity
-                  </p>
-                  <div className="mt-12">{food.qty}</div>
-                </div>
-                <div className=" flex flex-col items-center">
-                  <p className="font-bold font-cursive text-red-700 text-5xl">
-                    Option
-                  </p>
-                  <div className="mt-12">{food.size}</div>
-                </div>
-                <div className="flex flex-col items-center">
-                  <p className="font-bold font-cursive text-4xl text-red-700 text-5xl">
-                    Price
-                  </p>
-                  <div className="mt-12">{food.price}</div>
-                </div>
 
-                <div>
-                  <button type="button" className="btn mt-[90px]">
-                    {
-                      <AiFillDelete
-                        className="text-5xl fill-red-600"
-                        onClick={() => {
-                          dispatch({ type: "REMOVE", index: index });
-                        }}
-                      />
-                    }
-                  </button>{" "}
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div>
-          <h1 className="mt-10 text-5xl font-poppins font-bold">
-            Total Price: {totalPrice}/-
-          </h1>
-        </div>
-        <div>
-          <button
-            className="p-6 bg-red-400 text-4xl mt-10 font-bold rounded-xl text-white"
-            onClick={handleCheckOut}
-          >
-            {" "}
-            Check Out{" "}
-          </button>
-        </div>
+  return (
+    <div className="p-20">
+      <table className="table-auto w-full">
+        <thead>
+          <tr className="text-4xl">
+            <th className="px-4 py-2">No.</th>
+            <th className="px-4 py-2">Name</th>
+            <th className="px-4 py-2">Quantity</th>
+            <th className="px-4 py-2">Size</th>
+            <th className="px-4 py-2">Price</th>
+            <th className="px-4 py-2">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((food, index) => (
+            <tr key={index} className="text-center text-3xl pt-8 font-cursive">
+              <td className="border px-8 py-2 ">{index + 1}</td>
+              <td className="border px-4 py-2">{food.name}</td>
+              <td className="border px-4 py-2">{food.qty}</td>
+              <td className="border px-4 py-2">{food.size}</td>
+              <td className="border px-4 py-2 text-center">{food.price}</td>
+              <td className="border px-4 py-2 ">
+                <button type="button">
+                  <AiFillDelete
+                    className="text-4xl fill-red-600"
+                    onClick={() => {
+                      dispatch({ type: "REMOVE", index: index });
+                    }}
+                  />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="mt-16 text-5xl text-center font-poppins font-bold">
+        Total Price: {totalPrice}/-
+      </div>
+      <div className="flex justify-center">
+        <button
+          className="p-6 bg-red-400 text-4xl mt-10  font-bold rounded-xl text-white"
+          onClick={handleCheckOut}
+        >
+          Check Out
+        </button>
       </div>
     </div>
   );
